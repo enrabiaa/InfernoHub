8508fef24f40b9e4c4b0d06e784f7747cb8e7483
makima <113211848+enrabiaa@users.noreply.github.com>
2023-03-02 08:05:02 -0500

    Update InfernoHub.lua

--- InfernoHub.lua
+++ InfernoHub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/i4mitty/InfernoHub/main/InfernoHub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/i4mitty/InfernoHub/main/InfernoHub%20-%20Ragdoll%20Engine.lua"))()
